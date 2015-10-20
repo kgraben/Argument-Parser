20af529ab08cf28975a71f59b71a09a389e75d96
Kurtis Graben <kurtis.graben@me.com>
2015-10-20 16:36:22 -0500

    Refactoring
    Refactoring on ArgumentParser.java

--- main/java/edu/jsu/mcis/ArgumentParser.java
+++ main/java/edu/jsu/mcis/ArgumentParser.java
@@ -32,123 +32,162 @@ public class ArgumentParser{
   private UserArgumentException temp;
 
 	public ArgumentParser(){
-		namedArguments=new LinkedHashMap<String,String>();
-		positionalArguments=new LinkedHashMap<String,DATATYPE>();
-		intArguments=new LinkedHashMap<String,String>();
-		floatArguments=new LinkedHashMap<String,String>();
-		booleanArguments=new LinkedHashMap<String,String>();
-		stringArguments=new LinkedHashMap<String,String>();
-		argument=new Argument();
-		userInput=new ArrayList<String>();
+		namedArguments=new LinkedHashMap<String,Argument>();
+		positionalArguments=new LinkedHashMap<String,Argument>();
 		userPositionalArguments=new ArrayList<String>();
 		userNamedArguments=new ArrayList<String>();
-		data=DATATYPE.STRING;
 		helpMessage=0;
 		missingArgumentWidthAndHeightMessage=0;
 		missingArgumentHeightMessage=0;
 		unrecognizedArgumentsMessage=0;
 		incorrectDataTypeMessage=0;
-		temp= new UserArgumentException();
 	}
 
 	public void addPositionalArgument(String x){
-		positionalArguments.put(x,data);
+		Argument temp = new Argument();
+		temp.setDataType(Argument.DATATYPE.STRING);
+		temp.setPositionalName(x);
+		positionalArguments.put(x,temp);
 	}
 
-	public void addPositionalArgument(String x,DATATYPE d){
-		positionalArguments.put(x,d);
+	public void addPositionalArgument(String x,DATATYPE t){
+		Argument temp = new Argument();
+		temp.setDataType(t);
+		temp.setPositionalName(x);
+		positionalArguments.put(x,temp);
 	}
 
 	public void addNamedArgument(String x){
-		namedArguments.put(x,x);
+		Argument temp = new Argument();
+		temp.setnamedName(x);
+		namedArguments.put(x,temp);
 	}
 
 	public void checkUserInputSize() throws ArrayIndexOutOfBoundsException{
 		if(userPositionalArguments.size()==1 && (!userPositionalArguments.contains("--h") && !userPositionalArguments.contains("-h"))){
+			try{
 				missingArgumentWidthAndHeightMessage();
+				}
+			catch(NullPointerException ex){
+
 			}
+
+
+		}
+
+
+
 		else if(userPositionalArguments.size()==2){
-				missingArgumentHeightMessage();
+			try{
+				 missingArgumentHeightMessage();
 			}
+			catch(NullPointerException ex){
+
+			}
+		}
 		else if(userPositionalArguments.size()>positionalArguments.size()){
-			for(int m=3; m < userPositionalArguments.size(); m++){
-				temp.setUnrecognizedArgument(userPositionalArguments.get(m));
-			    unrecognizedArgumentsMessage();
-			}
-	 	}
+				for(int m=3; m < userPositionalArguments.size(); m++){
+					//temp.setUnrecognizedArgument(userPositionalArguments.get(m));
+					try{
+						 unrecognizedArgumentsMessage();
+				    	System.out.println("usage: java VolumeCalculator length width height" + "\n" + "VolumeCalcultor.java: error: unrecognized arguments: " + userPositionalArguments.get(m));
+				    }
+				    catch(NullPointerException ex){
+
+				    }
+				}
+
+		 }
 	}
 
 	public void matchNamedArguments(){
 		for(String s: namedArguments.keySet()){
-			String d=namedArguments.get(s);
-			if(d.equals("Type")){
-				String box="Box";
-				namedArguments.put(s,box);
-			}
-			else if(d.equals("Digits")){
-				String digit="4";
-				namedArguments.put(s,digit);
-			}
-			int j=1;
-			for(int i=0; i < userNamedArguments.size(); i+=2){
-				if((i % 2==0) && (j % 2==1)){
-					namedArguments.put(userNamedArguments.get(i), userNamedArguments.get(j));
-					j+=2;
+			Argument temp=namedArguments.get(s);
+
+				if(temp.getnamedName()=="Type"){
+					String box="Box";
+					temp.setNamedValue(box);
+					namedArguments.put(s,temp);
+
+				}
+
+				else if(temp.getnamedName()=="Digits"){
+					String digit="4";
+					temp.setNamedValue(digit);
+					namedArguments.put(s,temp);
 				}
 			}
-		}
+					int j=1;
+					for(int i=0; i < userNamedArguments.size(); i+=2){
+						Argument temp2=new Argument();
+						if((i % 2==0) && (j % 2==1)){
+							temp2.setNamedValue(userNamedArguments.get(j));
+							namedArguments.put(userNamedArguments.get(i),temp2);
+							j+=2;
+						}
+					}
 	}
 
 	public void matchPositionalArguments(){
 		int i=0;
 
-		if(userPositionalArguments.size()==positionalArguments.size()){
-			for(String s: positionalArguments.keySet()){
-				DATATYPE d=positionalArguments.get(s);
-				if(d==data.INT){
-					argument.setUserArg(userPositionalArguments.get(i));
-					try{
-						Integer.parseInt(argument.getUserArg());
-						intArguments.put(s,argument.getUserArg());
+			 if(userPositionalArguments.size()==positionalArguments.size()){
+
+
+					for(String s: positionalArguments.keySet()){
+						Argument temp=positionalArguments.get(s);
+
+						if(temp.getDataType()==Argument.DATATYPE.INT){
+							temp.setPositionalValue(userPositionalArguments.get(i));
+							try{
+
+								Integer.parseInt(temp.getPositionalValue());
+							}
+							catch(NumberFormatException ex){
+								temp.setIncorrectDataType(temp.getPositionalValue());
+								incorrectDataTypeMessage();
+							}
+
+							i++;
+						}
+
+						else if(temp.getDataType()==Argument.DATATYPE.FLOAT){
+							temp.setPositionalValue(userPositionalArguments.get(i));
+
+							try{
+
+								Float.parseFloat(temp.getPositionalValue());
+
+							}
+							catch(NumberFormatException ex){
+								temp.setIncorrectDataType(temp.getPositionalValue());
+								incorrectDataTypeMessage();
+
+							i++;
+						}
+
+						else if(temp.getDataType()==Argument.DATATYPE.BOOLEAN){
+							temp.setPositionalValue(userPositionalArguments.get(i));
+							i++;
+						}
+
+						else{
+							temp.setPositionalValue(userPositionalArguments.get(i));
+							i++;
+						}
 					}
-					catch(NumberFormatException ex){
-						temp.setIncorrectDataType(argument.getUserArg());
-						incorrectDataTypeMessage();
-					}
-					i++;
 				}
-				else if(d==data.FLOAT){
-					argument.setUserArg(userPositionalArguments.get(i));
-					try{
-						Float.parseFloat(argument.getUserArg());
-						floatArguments.put(s,argument.getUserArg());
-					}
-					catch(NumberFormatException ex){
-						temp.setIncorrectDataType(argument.getUserArg());
-						incorrectDataTypeMessage();
-					}
-					i++;
-				}
-				else if(d==data.BOOLEAN){
-					argument.setUserArg(userPositionalArguments.get(i));
-					booleanArguments.put(s,argument.getUserArg());
-					i++;
-				}
-				else{
-					argument.setUserArg(userPositionalArguments.get(i));
-					stringArguments.put(s,argument.getUserArg());
-					i++;
-				}
-			}
-		}
 	}
 
 	public void parse(String[] args){
 		for(int i=0; i<args.length; i++){
 			userPositionalArguments.add(args[i]);
 		}
+
 		for(int j=0; j < userPositionalArguments.size(); j++){
+
 			int k=0;
+
 			if(userPositionalArguments.contains("--h") ||userPositionalArguments.contains("-h") ){
 				getHelpMessage();
 				break;
@@ -159,40 +198,42 @@ public class ArgumentParser{
 				userPositionalArguments.remove(userPositionalArguments.get(j+1));
 				userPositionalArguments.remove(userPositionalArguments.get(j));
 				j=0;
-			}
 		}
-
+	}
 		checkUserInputSize();
 		matchPositionalArguments();
 		matchNamedArguments();
 	}
 
  	@SuppressWarnings("unchecked")
- 	public <T> T getValue(String name){
- 		for(String namedArg: namedArguments.keySet()){
-			if(name.equals(namedArg)){
-				return (T) namedArguments.get(name);
-			}
-		}
- 		for(String intName: intArguments.keySet()){
-			if(name.equals(intName)){
-				int num=Integer.parseInt(intArguments.get(name));
-				return (T) new Integer(num);
-			}
-		}
-		for(String booleanName: booleanArguments.keySet()){
-			if(name.equals(booleanName)){
-				boolean num=Boolean.parseBoolean(booleanArguments.get(name));
-				return (T) Boolean.valueOf(num);
-			}
-		}
-		for(String floatName: floatArguments.keySet()){
-			if(name.equals(floatName)){
-				float num=Float.parseFloat(floatArguments.get(name));
-				return (T) new Float(num);
-			}
-		}
-		return (T) stringArguments.get(name);
+	public <T> T getValue(String name){
+
+   		if(positionalArguments.get(name) != null){
+   			Argument temp=new Argument();
+   			temp= positionalArguments.get(name);
+   			if(temp.getDataType()==Argument.DATATYPE.INT){
+   				int num=Integer.parseInt(temp.getPositionalValue());
+   				return (T) new Integer (num);
+   			}
+
+   			else if(temp.getDataType()==Argument.DATATYPE.FLOAT){
+   				float num=Float.parseFloat(temp.getPositionalValue());
+   				return (T) new Float (num);
+   			}
+
+   			else if(temp.getDataType()==Argument.DATATYPE.BOOLEAN){
+   				boolean num=Boolean.parseBoolean(temp.getPositionalValue());
+   				return (T) Boolean.valueOf(num);
+   			}
+   			else{
+   				return (T) new String(temp.getPositionalValue());
+   			}
+   		}
+
+
+   			Argument temp= new Argument();
+   			temp=namedArguments.get(name);
+   			return (T) temp.getNamedValue();
  	}
 
  	public int getSizeOfHashMap(){
@@ -226,7 +267,6 @@ public class ArgumentParser{
 
  	public boolean isHelpMessageCalled(){
  		if(helpMessage>0){
- 			helpMessage=0;
  			return true;
  		}
  		else
