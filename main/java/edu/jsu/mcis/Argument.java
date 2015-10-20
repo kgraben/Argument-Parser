76d8fda7572de0c278b7aa29070c62daedc98947
Kurtis Graben <kurtis.graben@me.com>
2015-10-20 16:27:41 -0500

    Refactoring
    refactors argument.java

--- main/java/edu/jsu/mcis/Argument.java
+++ main/java/edu/jsu/mcis/Argument.java
@@ -3,19 +3,79 @@ package edu.jsu.mcis;
 
 public class Argument{
 
-	private String userArgument;
 	private String positionalValue;
+	private String positionalName;
+	private String namedValue;
+	private String namedName;
+	private String incorrectArgument="";
+	private String unrecognizedArgument="";
+	private String type;
+	private String digit;
+	public enum DATATYPE{INT,FLOAT,BOOLEAN,STRING};
+  public DATATYPE data;
 
 	public Argument(){
 		positionalValue="";
-		userArgument="";
+		positionalName="";
+		namedValue="";
+		type="";
+		digit="";
+		data=DATATYPE.STRING;
 	}
 
-	public void setUserArg(String s){
-		userArgument=s;
+	public void setPositionalValue(String s){
+		positionalValue=s;
 	}
 
-	public String getUserArg(){
-		return userArgument;
+	public String getPositionalValue(){
+		return positionalValue;
+	}
+
+	public void setDataType(DATATYPE T){
+		data=T;
+	}
+
+	public DATATYPE getDataType(){
+		return data;
+	}
+
+	public void setPositionalName(String s){
+		positionalName=s;
+	}
+
+	public String getPositionalName(){
+		return positionalName;
+	}
+
+	public void setnamedName(String s){
+		namedName=s;
+	}
+
+	public String getnamedName(){
+		return namedName;
+	}
+
+	public void setNamedValue(String s){
+		namedValue=s;
+	}
+
+	public String getNamedValue(){
+		return namedValue;
+	}
+
+	public void setIncorrectDataType(String name){
+		incorrectArgument=name;
+	}
+
+	public String getIncorrectDataType(){
+		return incorrectArgument;
+	}
+
+	public void setUnrecognizedArgument(String name){
+		unrecognizedArgument=name;
+	}
+
+	public String getUnrecognizedArgument(){
+		return unrecognizedArgument;
 	}
 }
