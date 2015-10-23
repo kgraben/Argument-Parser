b261f0c7c55485e71f128e3d54f030a043962a17
wanghuida0 <wanghuida0@163.com>
2015-10-23 11:35:10 -0500

    delet
    src and ArgumentParser

--- ArgumentParser/src/main/java/edu/jsu/mcis/IncorrectDataTypeException.java
+++
@@ -1,6 +0,0 @@
-package edu.jsu.mcis;
-public class IncorrectDataTypeException extends RuntimeException {
-    	public IncorrectDataTypeException (String message) {
-    	super (message);
-   	 }	
-}
