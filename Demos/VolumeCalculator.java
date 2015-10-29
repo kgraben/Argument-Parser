1ceba8456d0f974fac86d9bb1f96ff81022bf6c0
Christopher Burdette <cburdette@jsu.edu>
2015-10-29 17:49:53 -0500

    Demo file

---
+++ Demos/VolumeCalculator.java
@@ -0,0 +1,44 @@
+
+import edu.jsu.mcis.*;
+import java.util.*;
+
+	public class VolumeCalculator {
+
+		
+   		public static void main(String[] args){
+   		
+   			ArgumentParser Ap= new ArgumentParser();
+   			Ap.addPositionalArgument("length",Argument.DATATYPE.INT);
+			Ap.addPositionalArgument("width",Argument.DATATYPE.INT);
+			Ap.addPositionalArgument("height",Argument.DATATYPE.INT);
+			Ap.addNamedArgument("Type");
+			Ap.addNamedArgument("Color");
+		
+			Ap.parse(args);
+			int length= Ap.getValue("length");
+			int width= Ap.getValue("width");
+			int height= Ap.getValue("height");
+			String type=Ap.getValue("Type");
+			String color=Ap.getValue("Color");
+			
+			System.out.println("The length is : " + length);
+			System.out.println("The width is : " + width);
+			System.out.println("The height is : " + height);
+			System.out.println("The Type Value equals : " + type);
+			System.out.println("The Color Value equals : " + color);
+			
+
+   	
+   	
+   	
+   	}
+
+
+
+
+
+
+
+
+
+}
