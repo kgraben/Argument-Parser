f0807d11f9baf22f93802f5f6ea049355611dc36
Christopher Burdette <cburdette@jsu.edu>
2015-11-06 11:19:53 -0600

    updated

---
+++ main/java/edu/jsu/mcis/PositionalArgument.java
@@ -0,0 +1,35 @@
+package edu.jsu.mcis;
+
+public class PositionalArgument extends Argument{
+
+
+	private String description = "";
+	
+	
+	
+	public void setDescription(String description){
+		this.description=description;
+	
+	}
+	
+	
+	public String getDescription(){
+		return description;
+	
+	
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
