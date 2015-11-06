a87e96c27f3af96f0fc26c1bedf16678f573e43d
Christopher Burdette <cburdette@jsu.edu>
2015-11-06 11:18:15 -0600

    updated

--- main/java/edu/jsu/mcis/UnknownArgumentException.java
+++ main/java/edu/jsu/mcis/UnknownArgumentException.java
@@ -1,16 +1,21 @@
 package edu.jsu.mcis;
 
 public class UnknownArgumentException extends RuntimeException {
+	public UnknownArgumentException (String message) {
+    	super (message);
+	/*
 	private String argumentName;
 	public UnknownArgumentException(String argName) {
 		super("I do not recognize " + argumentName);
-    argumentName = argName;
+    	argumentName = argName;
   }
 
 	public String getArgumentName() {
 		return argumentName;
 	}
 
+	*/
+	}
 
 
 
