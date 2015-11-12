13797f5841fec5b4aa017cc15777f217ea306747
wanghuida0 <wanghuida0@163.com>
2015-11-12 17:49:31 -0600

    clean

--- main/java/edu/jsu/mcis/ArgumentParser.java
+++ main/java/edu/jsu/mcis/ArgumentParser.java
@@ -31,12 +31,12 @@ public class ArgumentParser{
 	}
 
 	public void addPositionalArgument(String name, Argument.Type type, String description) {
-		positionalList.add(name);
-  	PositionalArgument temp = new PositionalArgument();
-  	temp.setType(type);
-  	temp.setName(name);
-  	temp.setDescription(description);
-  	arguments.put(name, temp);
+        positionalList.add(name);
+        PositionalArgument temp = new PositionalArgument();
+        temp.setType(type);
+        temp.setName(name);
+        temp.setDescription(description);
+        arguments.put(name, temp);
 	}
 
 	public void addNamedArgument(String name, String shortName, Argument.Type type, String defaultValue) {
@@ -51,9 +51,9 @@ public class ArgumentParser{
 
 	public void parse(String[] args) {
 		List<String> userArgs = new ArrayList<String>();
-  	for(int i=0; i<args.length; i++){
-  		userArgs.add(args[i]);
-  	}
+        for(int i=0; i<args.length; i++){
+            userArgs.add(args[i]);
+        }
 		if(userArgs.contains("-h") || userArgs.contains("--help") ){
 			userArgs.remove("--help");
 			userArgs.remove("-h");
@@ -62,18 +62,17 @@ public class ArgumentParser{
 		}
 		for(int i = 0; i < userArgs.size(); i++) {
 			String arg = userArgs.get(i);
-
-  		if(arg.startsWith("--") || arg.startsWith("-")) {
-    		String name = (arg.startsWith("--"))? arg.substring(2) : arg.substring(1);
-    		if(name.equals("h")){
-    			userArgs.remove("h");
-    			throw new HelpMessageException(getHelpMessage());
-    		}
-    		Argument a = arguments.get(name);
-    		a.setValue(userArgs.get(i+1));
-    		arguments.put(userArgs.get(i),a);
-    		userArgs.set(i, "");
-        userArgs.set(i+1, "");
+            if(arg.startsWith("--") || arg.startsWith("-")) {
+                String name = (arg.startsWith("--"))? arg.substring(2) : arg.substring(1);
+                if(name.equals("h")){
+                    userArgs.remove("h");
+                    throw new HelpMessageException(getHelpMessage());
+                }
+                Argument a = arguments.get(name);
+                a.setValue(userArgs.get(i+1));
+                arguments.put(userArgs.get(i),a);
+                userArgs.set(i, "");
+                userArgs.set(i+1, "");
 			}
    		}
     	List<String> posArgs = new ArrayList<String>();
@@ -92,32 +91,32 @@ public class ArgumentParser{
             buildPositionalArguments();
  	}
 
-  private String buildArgumentUsage() {
-    String s = "";
-    for(String name : arguments.keySet()) {
-      s += "[" + name + "]";
+    private String buildArgumentUsage() {
+        String s = "";
+        for(String name : arguments.keySet()) {
+            s += "[" + name + "]";
+        }
+        return s;
     }
-    return s;
-  }
 
 	protected String getMissingArguments() {
 	   return buildArgumentUsage();
-  }
+    }
 
 	private String buildPositionalArguments() {
-    String s = "";
-    for(String name : arguments.keySet()) {
-      Argument a = arguments.get(name);
-      s += a.getName() + " " + a.getDescription() + " (" + a.getType() + ")\n";
+        String s = "";
+        for(String name : arguments.keySet()) {
+            Argument a = arguments.get(name);
+            s += a.getName() + " " + a.getDescription() + " (" + a.getType() + ")\n";
+        }
+        return s.substring(0, s.length()-1);
     }
-    return s.substring(0, s.length()-1);
-  }
 
 	@SuppressWarnings("unchecked")
 	public <T> T getValue(String name){
-    Argument arg = arguments.get(name);
+        Argument arg = arguments.get(name);
 		if(arg != null) {
-      	String val = arg.getValue();
+            String val = arg.getValue();
 			if(arg.getType()==Argument.Type.INT){
 				int num = Integer.parseInt(val);
 				return (T) new Integer (num);
@@ -134,14 +133,14 @@ public class ArgumentParser{
 				return (T) new String(val);
 			}
 		}
-  	else {
+        else {
     		throw new UnknownArgumentException(name);
-  	}
+        }
 	}
 
 	private void checkUserInputSize(List<String> list) {
 		if(list.size()==1){
-				throw new MissingArgumentException("usage: java VolumeCalculator length width heigh. VolumeCalculator.java: error: the following arguments are required: width height");
+			throw new MissingArgumentException("usage: java VolumeCalculator length width heigh. VolumeCalculator.java: error: the following arguments are required: width height");
 		}
 		else if(list.size()==2){
 			throw new MissingArgumentException("usage: java VolumeCalculator length width heigh. VolumeCalculator.java: error: the following arguments are required: width height");
@@ -150,52 +149,52 @@ public class ArgumentParser{
 
 	private void checkUserDataType(List<String> list){
 		for(int i = 0; i < list.size(); i++) {
-  		try {
-  			String positional = positionalList.get(i);
-  			for(String name : arguments.keySet()) {
-   				Argument a = arguments.get(name);
-   				if(positional == a.getName()) {
-            a.setValue(list.get(i));
-            if(a.getType() == Argument.Type.INT){
-              try {
-                int num = Integer.parseInt(a.getValue());
-                arguments.put(name,a);
-              }
-              catch(NumberFormatException e) {
-                throw new IncorrectDataTypeException(getIncorrectDataTypeMessage(name, list.get(i), a.getType().toString()));
-              }
+            try {
+                String positional = positionalList.get(i);
+                for(String name : arguments.keySet()) {
+                    Argument a = arguments.get(name);
+                    if(positional == a.getName()) {
+                        a.setValue(list.get(i));
+                        if(a.getType() == Argument.Type.INT){
+                            try {
+                                int num = Integer.parseInt(a.getValue());
+                                arguments.put(name,a);
+                            }
+                            catch(NumberFormatException e) {
+                                throw new IncorrectDataTypeException(getIncorrectDataTypeMessage(name, list.get(i), a.getType().toString()));
+                            }
+                        }
+                        else if(a.getType()==Argument.Type.FLOAT){
+                            try {
+                                float num = Float.parseFloat(a.getValue());
+                                arguments.put(name,a);
+                            }
+                            catch(NumberFormatException e){
+                                throw new IncorrectDataTypeException(getIncorrectDataTypeMessage(name, list.get(i), a.getType().toString()));
+                            }
+                        }
+                        else if(a.getType()==Argument.Type.BOOLEAN){
+                            try{
+                                boolean num = Boolean.parseBoolean(a.getValue());
+                                arguments.put(name,a);
+                            }
+                            catch(NumberFormatException e){
+                                throw new IncorrectDataTypeException(getIncorrectDataTypeMessage(name, list.get(i), a.getType().toString()));
+                            }
+                        }
+                    }
+                }
             }
-            else if(a.getType()==Argument.Type.FLOAT){
-              try {
-                float num = Float.parseFloat(a.getValue());
-                arguments.put(name,a);
-              }
-              catch(NumberFormatException e){
-                throw new IncorrectDataTypeException(getIncorrectDataTypeMessage(name, list.get(i), a.getType().toString()));
-              }
+            catch(IndexOutOfBoundsException e){
+                throw new UnknownArgumentException("usage: java VolumeCalculator [length][width][height]\nVolumeCalculator.java: error: unrecognized arguments: " + list.get(i));
             }
-            else if(a.getType()==Argument.Type.BOOLEAN){
-              try{
-                boolean num = Boolean.parseBoolean(a.getValue());
-                arguments.put(name,a);
-              }
-              catch(NumberFormatException e){
-                throw new IncorrectDataTypeException(getIncorrectDataTypeMessage(name, list.get(i), a.getType().toString()));
-              }
-            }
-          }
         }
-      }
-      catch(IndexOutOfBoundsException e){
-        throw new UnknownArgumentException("usage: java VolumeCalculator [length][width][height]\nVolumeCalculator.java: error: unrecognized arguments: " + list.get(i));
-      }
     }
-  }
 
  	private String getIncorrectDataTypeMessage(String argName, String incorrectValue, String type) {
-    return "usage: java " + programName + " " + buildArgumentUsage() + "\n" +
-    programName + ".java: error: argument " + argName + ": invalid " + type
-    + " value: " + incorrectValue;
+        return "usage: java " + programName + " " + buildArgumentUsage() + "\n" +
+        programName + ".java: error: argument " + argName + ": invalid " + type
+        + " value: " + incorrectValue;
  	}
 
 	protected String getProgramName(){
--- main/java/edu/jsu/mcis/XML.java
+++ main/java/edu/jsu/mcis/XML.java
@@ -10,72 +10,72 @@ import java.io.File;
 
 public class XML {
 
-  private String fileName;
-  public String output = "";
+    private String fileName;
+    public String output = "";
 
-  public void setFileName(String fileName){
-    this.fileName = fileName;
-    try{
-      File file = new File(fileName);
-      DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-      DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-      Document doc = dBuilder.parse(file);
-    }
-    catch (Exception e){
-      throw new FileErrorException("Not Found: " + fileName);
-    }
-  }
-
-  protected String getFileName(){
-    return fileName;
-  }
-
-  public String loadXML() {
-    try {
-      File xmlFile = new File(getFileName());
-      DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-      DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-      Document doc = dBuilder.parse(xmlFile);
-      doc.getDocumentElement().normalize();
-      NodeList nList = doc.getElementsByTagName("positional");
-      for (int temp = 0; temp < nList.getLength(); temp++) {
-        Node nNode = nList.item(temp);
-        if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-          Element eElement = (Element) nNode;
-          output = output + ("\n||" + "Arguments : " + eElement.getElementsByTagName("name").item(0).getTextContent() +
-          "\ntype : " + eElement.getElementsByTagName("type").item(0).getTextContent() +
-          "\ndescription : " + eElement.getElementsByTagName("description").item(0).getTextContent() +
-          "\nvalue : " + eElement.getElementsByTagName("position").item(0).getTextContent() + "||\n");
+    public void setFileName(String fileName){
+        this.fileName = fileName;
+        try{
+        File file = new File(fileName);
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        Document doc = dBuilder.parse(file);
         }
-      }
-      NodeList list = doc.getElementsByTagName("named");
-      for (int temp = 0; temp < list.getLength(); temp++){
-        Node nNode = list.item(temp);
-        if(nNode.getNodeType() == Node.ELEMENT_NODE) {
-          Element eElement = (Element) nNode;
-          output = output + ("\n||" + "Type : " + eElement.getElementsByTagName("name").item(0).getTextContent() +
-          "\nshortname : " + eElement.getElementsByTagName("shortname").item(0).getTextContent() +
-          "\ndata type : " + eElement.getElementsByTagName("type").item(0).getTextContent() +
-          "\ndefault : " + eElement.getElementsByTagName("default").item(0).getTextContent() + "||\n");
+        catch (Exception e){
+            throw new FileErrorException("Not Found: " + fileName);
         }
-      }
     }
-    catch (Exception e) {
-      throw new FileErrorException("Not Found: " + getFileName());
+
+    protected String getFileName(){
+        return fileName;
     }
-    return output;
-  }
 
-  public String returnLoad(){
-    return loadXML();
-  }
-
-  public void saveXML() {
-    try{
-
+    public String loadXML() {
+        try {
+            File xmlFile = new File(getFileName());
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(xmlFile);
+            doc.getDocumentElement().normalize();
+            NodeList nList = doc.getElementsByTagName("positional");
+            for (int temp = 0; temp < nList.getLength(); temp++) {
+                Node nNode = nList.item(temp);
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) nNode;
+                    output = output + ("\n||" + "Arguments : " + eElement.getElementsByTagName("name").item(0).getTextContent() +
+                    "\ntype : " + eElement.getElementsByTagName("type").item(0).getTextContent() +
+                    "\ndescription : " + eElement.getElementsByTagName("description").item(0).getTextContent() +
+                    "\nvalue : " + eElement.getElementsByTagName("position").item(0).getTextContent() + "||\n");
+                }
+            }
+            NodeList list = doc.getElementsByTagName("named");
+            for (int temp = 0; temp < list.getLength(); temp++){
+                Node nNode = list.item(temp);
+                if(nNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) nNode;
+                    output = output + ("\n||" + "Type : " + eElement.getElementsByTagName("name").item(0).getTextContent() +
+                    "\nshortname : " + eElement.getElementsByTagName("shortname").item(0).getTextContent() +
+                    "\ndata type : " + eElement.getElementsByTagName("type").item(0).getTextContent() +
+                    "\ndefault : " + eElement.getElementsByTagName("default").item(0).getTextContent() + "||\n");
+                }
+            }
+        }
+        catch (Exception e) {
+            throw new FileErrorException("Not Found: " + getFileName());
+        }
+        return output;
     }
-    catch (Exception e) {
 
+    public String returnLoad(){
+        return loadXML();
     }
-  }
+
+    public void saveXML() {
+        try{
+
+        }
+        catch (Exception e) {
+
+        }
+    }
 }
