b3b2af5ad46c5a9948086df0c607ee3c0b635b2a
wanghuida0 <wanghuida0@163.com>
2015-12-01 16:37:57 -0600

    documentation

--- main/java/edu/jsu/mcis/IncorrectDataTypeException.java
+++ main/java/edu/jsu/mcis/IncorrectDataTypeException.java
@@ -1,5 +1,16 @@
 package edu.jsu.mcis;
+/**
+ * Class description: Exception class for Incorrect type. Extend RuntimeException class.
+ *
+ *
+ *
+ */
 public class IncorrectDataTypeException extends RuntimeException {
+    /**
+     * Constructs a new IncorrectDataTypeException with a detail message
+     * @Param message - the detail message.
+     *
+     */
     	public IncorrectDataTypeException (String message) {
     	super (message);
    	 }	
