a98a711ba6bdf4ef3a0cf574f6b52f1f6bb9de4e
wanghuida0 <wanghuida0@163.com>
2015-12-01 16:38:41 -0600

    documentation

--- main/java/edu/jsu/mcis/MissingArgumentException.java
+++ main/java/edu/jsu/mcis/MissingArgumentException.java
@@ -1,7 +1,16 @@
 package edu.jsu.mcis;
-
+/**
+ * Class description: Exception class for Missing Argument. Extend RuntimeException class.
+ *
+ *
+ *
+ */
 public class MissingArgumentException extends RuntimeException {
-
+     /**
+     * Constructs a new MissingArgumentException with a detail message
+     * @Param message - the detail message.
+     *
+     */
     public MissingArgumentException (String message) {
     	super (message);
    	 }
