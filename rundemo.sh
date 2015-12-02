61ede4fc1cd12b06a8b2a973849e8f8bdb8813fc
Christopher Burdette <cburdette@jsu.edu>
2015-12-01 20:51:35 -0600

    updated

--- rundemo.sh
+++ rundemo.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 cd Demos
 javac -cp .:../build/classes/main VolumeCalculator.java
-java -cp .:../build/classes/main:/Users/burdettec/  VolumeCalculator 7 3 --Color Blue 8 
+java -cp .:../build/classes/main:/Users/burdettec/  VolumeCalculator 8 9 10 --Color pink --Type hexatagon 
 cd ..
