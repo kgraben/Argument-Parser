7906055c61b1e3242324c2caf37752061b8cc5d2
Christopher Burdette <cburdette@jsu.edu>
2015-12-01 20:52:12 -0600

    updated

--- Demos/VolumeCalculator.java
+++ Demos/VolumeCalculator.java
@@ -8,19 +8,22 @@ import java.util.*;
    		public static void main(String[] args){
    		
    			ArgumentParser Ap= new ArgumentParser();
+   			XML xml = new XML();
    			Ap.assignProgramName("Volume Calculator");
    			Ap.addPositionalArgument("length",Argument.Type.INT);
 			Ap.addPositionalArgument("width",Argument.Type.INT);
 			Ap.addPositionalArgument("height",Argument.Type.INT);
 			Ap.addNamedArgument("Type", "t",Argument.Type.STRING, "Box");
 			Ap.addNamedArgument("Color", "c",Argument.Type.STRING, "yellow");
-		
+			
+			
+			xml.saveXML("demoXML.xml",Ap);
 			Ap.parse(args);
 			int length= Ap.getValue("length");
 			int width= Ap.getValue("width");
 			int height= Ap.getValue("height");
 			String type=Ap.getValue("Type");
-			String color=Ap.getValue("Color");
+			String car=Ap.getValue("c");
 			
 			
 			System.out.println("The length is : " + length);
@@ -28,6 +31,7 @@ import java.util.*;
 			System.out.println("The height is : " + height);
 			System.out.println("The Type Value equals : " + type);
 			System.out.println("The Color Value equals : " + color);
+		
 			
 
    	
